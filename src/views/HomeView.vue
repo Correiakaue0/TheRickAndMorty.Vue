4549ca1b2f087ceaf743cff8c85bb768e00d06e8
Kaue Correia <Correiakaue0>
2023-09-14 20:47:44 -0300

    [Kaue] - requisição

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,15 @@
 <script setup>
+import { onMounted, reactive, ref } from 'vue';
+
+
+onMounted(() => {
+  fetch('https://rickandmortyapi.com/api/character')
+  .then(response => response.json())
+  .then(response => {
+    console.log(response);
+  })
+})
+
 </script>
 
 <template>
